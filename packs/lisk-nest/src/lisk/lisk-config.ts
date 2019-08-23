9288b745ffaad3bd7a646930077ef532e7ea1e6a
raziel <eu@mail.com>
2019-08-23 20:33:05 +0300

    upping to the date

--- packs/lisk-nest/src/lisk/lisk-config.ts
+++ packs/lisk-nest/src/lisk/lisk-config.ts
@@ -2,6 +2,6 @@ import { Application, genesisBlockDevnet, configDevnet } from 'lisk-sdk';
 
 configDevnet.app.label = 'jackal-blockchain'; 
 configDevnet.components.storage.password = 'password';
-configDevnet.components.logger.consoleLogLevel = 'error';
+configDevnet.components.logger.consoleLogLevel = 'info';
 
 export const lisk = new Application(genesisBlockDevnet, configDevnet);
