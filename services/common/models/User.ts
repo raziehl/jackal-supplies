930bf9013ab95ba69ede301542c1cc8beee82772
raziehl <matei.petroschi@gmail.com>
2020-03-07 11:28:02 +0200

    login account -> user

--- services/common/models/User.ts
+++ services/common/models/User.ts
@@ -1,50 +1,5 @@
 import { Asset } from './Asset';
 
-export class UserInfo {
-    username: string;
-
-    constructor(userInfo: Partial<UserInfo> = {}) {
-        this.username = userInfo.username || '';
-    }
-}
-
-export class AssetBlock {
-    userInfo?: UserInfo;
-    portfolio?: Array<Asset>;
-
-    constructor(asset: Partial<AssetBlock> = {}) {
-        this.userInfo = new UserInfo(asset.userInfo);
-        this.portfolio = new Array<Asset>();
-        if(asset.portfolio)
-            asset.portfolio.forEach(asset => {
-                this.portfolio.push(new Asset(asset))
-            });
-        else this.portfolio = new Array<Asset>();
-    }
-}
-
-export class Delegate {
-    username: string;
-    vote: string;
-    rewards: string;
-    producedBlocks: number;
-    missedBlocks: number;
-    rank: number;
-    productivity: number;
-    approval: number;
-
-    constructor(delegate: Partial<Delegate> = {}) {
-        this.username = delegate.username || "";
-        this.vote = delegate.vote || "";
-        this.rewards = delegate.rewards || "";
-        this.producedBlocks = delegate.producedBlocks || 0;
-        this.missedBlocks = delegate.missedBlocks || 0;
-        this.rank = delegate.rank || 0;
-        this.productivity = delegate.productivity || 0;
-        this.approval = delegate.approval || 0;
-    }
-}
-
 export class Account { 
     address: string;
     publicKey: string;
@@ -73,6 +28,51 @@ export class User extends Account {
         this.passphrase = user.passphrase || '';
         this.expiresIn = user.expiresIn || 28800000;
     }
+}
+
+export class UserInfo {
+  username: string;
+
+  constructor(userInfo: Partial<UserInfo> = {}) {
+      this.username = userInfo.username || '';
+  }
+}
+
+export class AssetBlock {
+  userInfo?: UserInfo;
+  portfolio?: Array<Asset>;
+
+  constructor(asset: Partial<AssetBlock> = {}) {
+      this.userInfo = new UserInfo(asset.userInfo);
+      this.portfolio = new Array<Asset>();
+      if(asset.portfolio)
+          asset.portfolio.forEach(asset => {
+              this.portfolio.push(new Asset(asset))
+          });
+      else this.portfolio = new Array<Asset>();
+  }
+}
+
+export class Delegate {
+  username: string;
+  vote: string;
+  rewards: string;
+  producedBlocks: number;
+  missedBlocks: number;
+  rank: number;
+  productivity: number;
+  approval: number;
+
+  constructor(delegate: Partial<Delegate> = {}) {
+      this.username = delegate.username || "";
+      this.vote = delegate.vote || "";
+      this.rewards = delegate.rewards || "";
+      this.producedBlocks = delegate.producedBlocks || 0;
+      this.missedBlocks = delegate.missedBlocks || 0;
+      this.rank = delegate.rank || 0;
+      this.productivity = delegate.productivity || 0;
+      this.approval = delegate.approval || 0;
+  }
 }
 
 export interface Credential {
